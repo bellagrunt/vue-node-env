7474bd4c5f833b16ede803578d35eadea349d25a
Rogue Bell <mattbellantoni3@gmail.com>
2019-10-18 13:45:17 +0200

    adding the node index express file

--- environment.sh
+++ environment.sh
@@ -101,6 +101,14 @@ function install_yarn {
 ### EXAMPLE PROJECT
 #####################
 
+# function create_test {
+#   projects_directory=~/projects
+#   if [ ! -d "$projects_directory" ]; then
+#   mkdir "projects_directory"
+#   echo "${projects_directory} created successfully"
+#   fi
+# }
+
 function create_project {
   echo "Creating example project"
   # Ensure project exists
@@ -144,7 +152,71 @@ function create_backend_structure {
 
 function create_index_file {
   echo "Configuring index file"
-  echo "const express = require('express');" >> index.js
+  echo "const express = require('express');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+// require("dotenv").config();
+require('dotenv').config({
+  path: './.env',
+});
+
+// cookie parser for auth token
+app.use(cookieParser())
+// routes
+app.use(cors());
+const { apiRoutes } = require('./routes/index');
+
+const mongoose = require('mongoose');
+const {
+  PORT,
+  NODE_ENV,
+  MONGO_DB_URI,
+  DB_NAME,
+  DB_NAME_TEST,
+} = process.env;
+
+const dbName = NODE_ENV === 'test' ? DB_NAME_TEST : DB_NAME;
+mongoose
+  .connect(`${MONGO_DB_URI}/${dbName}`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connection Successful'))
+  .catch(err => console.log(err));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use('/api', apiRoutes);
+
+const staticFileMiddleware = express.static(path.join(__dirname));
+
+app.use(staticFileMiddleware);
+
+app.get('/', function(req, res) {
+  res.render(path.join(__dirname + '/index.html'));
+});
+
+app.use(
+  '/public',
+  express.static(path.join(__dirname, '..', 'public')),
+);
+if (NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, '..', 'dist')));
+}
+
+if (NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+module.exports = app;
+
+  " >> index.js
+
 }
 
 
@@ -156,6 +228,7 @@ function main {
   # VERSION
   # install_node_env
   # install_node
+  # create_test
   create_project
   create_backend_structure
   create_index_file
